0f9fe5432849aea2239ee6e688ee134351181997
Tomi <141858115+tomiBraunn@users.noreply.github.com>
2025-06-17 09:06:58 -0300

    terminado

---
+++ For 4C/Assets/CubosManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubosManager : MonoBehaviour
+{
+    public CuboScript [] cuboScripts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cuboScripts.Length; i++)
+        {
+            Debug.Log(cuboScripts[0].esPersistente);
+            if (cuboScripts[i].esPersistente != true)
+            {
+                Destroy(cuboScripts[i].gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ For 4C/Assets/RepasoFor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepasoFor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 3; i < 100; i += 3)
+        {
+            Debug.Log(i);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
